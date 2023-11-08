db595ec45145f4fe4d4fd703672f10b0f0c4edef
Andy Shen <aashen@berkeley.edu>
2023-11-07 16:53:27 -0800

    compute rmpw weights

--- lalonde-demo.R
+++ lalonde-demo.R
@@ -4,7 +4,8 @@ library(tidyverse)
 data("lalonde")
 
 lalonde <- lalonde %>% 
-  mutate(race = ifelse(black == 0 & hisp == 0, "white", "not_white")) 
+  mutate(race = ifelse(black == 0 & hisp == 0, "white", "not_white")) %>% 
+  tibble()
 # mutate with w0, w1, w_rmpw
 
 # A^y: age, married
@@ -25,6 +26,17 @@ lalonde %>%
 lalonde %>% group_by(treat) %>% 
   summarise(mean(re78))
 
+pscore <- glm(black ~ age + married, family = "binomial", data = lalonde)$fitted.values
 
+prob_m_g0_A <- glm(treat ~ age + married, family = "binomial", data = lalonde, weights = 1 - black)$fitted.values
 
+prob_m_g1_AN <- glm(treat ~ age + married + educ + nodegr, family = "binomial", data = lalonde, weights = black)$fitted.values
+
+lalonde <- lalonde %>% mutate(w0 = mean(black == 0) / (1 - pscore), 
+                              w1 = mean(black == 1) / pscore, 
+                              w_rmpw = w1 * prob_m_g1_AN / prob_m_g0_A)
+
+View(lalonde)
+
+mu_10 <- 
 
